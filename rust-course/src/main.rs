0c8206ba8e17cac1305861aa2c2e27bd1e1d8273
Abdüssamed Erkalp <sameter-147@hotmail.com>
2026-05-21 23:02:46 +0300

    Add comments to main.rs
    Add an inline comment after the PI println and insert an empty block comment in main.rs. These changes are non-functional and only add annotations.

--- rust-course/src/main.rs
+++ rust-course/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     let age = 30;
     println!("YAS: {}", age);
     const PI: f32 = 3.14159;
-    println!("Pi is : {}", PI);
+    println!("Pi is : {}", PI); //yorum
     println!("Hello, cargo!");
+    /*
+    
+     */
 }
